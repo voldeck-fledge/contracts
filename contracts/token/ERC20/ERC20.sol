d41a09ccb47b92b42ea9ace7d9d1e880c4dd5c3d
voldeck-fledge <57817309+voldeck-fledge@users.noreply.github.com>
2021-04-08 21:29:59 -0400

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -1,4 +1,4 @@
-de// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
@@ -208,7 +208,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * - `recipient` cannot be the zero address.
      * - `sender` must have a balance of at least `amount`.
      */
-   function _transfer(address sender, address recipient, uint256 amount) internal override {
+   function _transfer(address sender, address recipient, uint256 amount) internal virtual {
     require(sender != address(0), "ERC20: transfer from the zero address");
     require(recipient != address(0), "ERC20: transfer to the zero address");
 
