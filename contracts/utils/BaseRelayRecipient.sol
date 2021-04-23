11b81397e697ef5ad5ec4106728f37380776a466
voldeck-fledge <57817309+voldeck-fledge@users.noreply.github.com>
2021-04-23 10:58:38 -0400

    Update BaseRelayRecipient.sol

--- contracts/utils/BaseRelayRecipient.sol
+++ contracts/utils/BaseRelayRecipient.sol
@@ -25,7 +25,7 @@ abstract contract BaseRelayRecipient is IRelayRecipient {
      * otherwise, return `msg.sender`.
      * should be used in the contract anywhere instead of msg.sender
      */
-    /*function _msgSender() internal override virtual view returns (address) {
+    function _msgSender() internal override virtual view returns (address) {
         if (msg.data.length >= 20 && isTrustedForwarder(msg.sender)) {
             // At this point we know that the sender is a trusted forwarder,
             // so we trust that the last bytes of msg.data are the verified sender address.
@@ -36,7 +36,7 @@ abstract contract BaseRelayRecipient is IRelayRecipient {
         } else {
             return msg.sender;
         }
-    }*/
+    }
 
     /**
      * return the msg.data of this call.
